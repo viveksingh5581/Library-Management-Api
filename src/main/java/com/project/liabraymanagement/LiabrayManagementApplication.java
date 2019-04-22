5571789152284c3c54aead5915aeddf00b82302f
Rituj Kumar <rituj.kumar@tesco.com>
2019-04-22 16:52:12 +0530

    Initaial commit

---
+++ src/main/java/com/project/liabraymanagement/LiabrayManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.liabraymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiabrayManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiabrayManagementApplication.class, args);
+	}
+
+}
